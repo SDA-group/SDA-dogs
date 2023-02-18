9f651d2547052d03bb62f12ace97a0b780698172
Sebastian Sioch <sebastian.sioch@gmail.com>
2023-02-18 16:44:50 +0100

    Poprawiony Search

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -19,7 +19,7 @@ const SearchPage = () => {
     useEffect(() => {
         // if (keyword !== "") {
 
-        axios.get(`https://api.thedogapi.com/v1/breeds`, {headers:{
+        axios.get(`https://api.thedogapi.com/v1/breeds/`, {headers:{
             'x-api-key' : 'live_G8dec3y62jgdWN9nRCrB2uzkGTHNQYVL0EkcsEqn7WqETR6JJTfdtjtqn8iRwX3k'
         }})
         .then((data) => {
